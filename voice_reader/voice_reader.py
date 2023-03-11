2020b47d4168af5072e3896fa8628131572fd122
Rohan Arni <50297222+rtenacity@users.noreply.github.com>
2023-03-10 23:52:55 -0500

    added basic reading functionality

--- voice_reader/voice_reader.py
+++ voice_reader/voice_reader.py
@@ -11,6 +11,7 @@ coins = ["tossup", "bonus"]
 p1 = qbreader.set_list()
 p1.remove('2016 "stanford housewrite"')
 packets = p1
+
 class State(pc.State):
 
     question_type = "tossup"
@@ -24,8 +25,10 @@ class State(pc.State):
     question = ""
     packet_questions = {}
     question_word_list:list = []
-    question_word_list2:list = []
-
+    on_screen:list = []
+    start: bool = False
+    x = -1
+    
     def question_logic(self):
         try:
             str_list = self.difficulty_str.split("-")
@@ -39,10 +42,28 @@ class State(pc.State):
             self.question = qbreader.random_question(questionType=self.question_type, difficulties=self.difficulty)
             print(self.question)
             self.question_word_list = self.question[0]['question'].split(" ")
-        
+
+    @pc.var
+    def reader(self):
+        try:
+            self.x+=1
+            self.on_screen.append(self.question_word_list[self.x])
+            return " ".join(self.on_screen)
+        except:
+            pass
+
     
+    async def tick(self):
+        """Update the clock every second."""
+        if self.start:
+            await asyncio.sleep(0.3)
+            return self.tick
 
-
+    def flip_switch(self, start):
+        """Start or stop the clock."""
+        self.start = start
+        if self.start:
+            return self.tick
 
 def index():
     return pc.center(
@@ -77,12 +98,15 @@ def index():
                 shadow="md",
                 on_click=State.question_logic,
             ),
+            pc.switch(is_checked=State.start, on_change=State.flip_switch),
+            
             bg="white",
             padding="2em",
             shadow="md",
             border_radius="lg",
             width="50%",
         ),
+        pc.text(State.reader),
         width="100%",
         height="100vh",
         bg="radial-gradient(circle at 22% 11%,rgba(62, 180, 137,.20),hsla(0,0%,100%,0) 19%)",
